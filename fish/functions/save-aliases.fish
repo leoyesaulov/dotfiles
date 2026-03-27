e0442219e674730d21f315216d237ce4e15a33b2
Leo Yesaulov <leo@omniserv.me>
2026-03-27 13:12:01 +0100

    (auto) Update fish functions

--- fish/functions/save-aliases.fish
+++ fish/functions/save-aliases.fish
@@ -1,4 +1,7 @@
 function save-aliases
+	# save cwd
+	set cwd $(pwd)
+
 	for file in ~/.config/fish/functions/*;
 		cp "$file" "/home/omni/repos/dotfiles/fish/functions/$(path basename $file)"
 	end;
@@ -8,4 +11,7 @@ function save-aliases
 	cd ~/repos/dotfiles/
 	git add fish/functions/
 	git commit -m "(auto) Update fish functions"
+
+	# return to the source directory
+	cd cwd
 end
