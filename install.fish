01a2da3249e4667c7b275320a3683bbd121d4054
Leo Yesaulov <leo@omniserv.me>
2026-03-29 11:01:40 +0200

    upd install script to update hypr configs

--- install.fish
+++ install.fish
@@ -1,11 +1,24 @@
 echo -e "\ninstall.fish has been called"
 set cwd $(pwd)
 
+# copy fish functions into .config/fish
 for file in ~/repos/dotfiles/fish/functions/*;
 	cp $file "~/.config/fish/functions/$(path basename $file)"
 end;
 
+# install dependencies from modules.md
 install-deps
+
+# backup old hyprland config files
+mkdir ~/.config/hypr/old-confs
+for file in ~/.config/hypr/*;
+	mv $file "~/.config/hypr/old-confs/$(path basename $file)"
+end;
+
+# copy linking config files into system configuration directory
+for file in ~/repos/dotfiles/hypr/links/*;
+	cp $file "~/.config/hypr/$(path basename $file)"
+end;
 	
 
 cd $cwd
